df39c73d102c80d652be8f98913505f846af0086
nahomjc <aslandjc7@gmail.com>
2022-12-21 22:39:13 -0800

    Name

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5]'> Clint</span>
+            Hi, I&#39;m <span className='text-[#5651e5]'> Nahom</span>
           </h1>
         <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
             
